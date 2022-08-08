287aa5db8b2a6e1153517d35a900c94e926f559a
Alvaro Gabriel Gomez <alvarogabrielgomez@gmail.com>
2022-08-08 12:52:19 -0300

    ci: removing .env to build on pipeline

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:alice/alice.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:quinientas_historias/core/utils/constants.dart';
 
 import 'core/app.dart';
@@ -11,25 +10,13 @@ import 'core/helpers/alice_helper.dart';
 void main() async {
   GlobalKey<NavigatorState>? navigateKey;
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
 
   if (FeatureFlag.aliceEnabled) {
     final Alice alice = AliceHelper.instance;
     navigateKey = alice.getNavigatorKey();
   }
 
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-        options: FirebaseOptions(
-      apiKey: dotenv.get('FIREBASE_API_KEY'),
-      appId: dotenv.get('FIREBASE_APP_ID'),
-      messagingSenderId: dotenv.get('FIREBASE_MESSAGING_SENDER_ID'),
-      projectId: dotenv.get('FIREBASE_PROJECT_ID'),
-      storageBucket: dotenv.get('FIREBASE_STORAGE_BUCKET'),
-    ));
-  } else {
-    await Firebase.initializeApp();
-  }
+  await Firebase.initializeApp();
   runApp(Application(
     navigatorKey: navigateKey,
   ));
