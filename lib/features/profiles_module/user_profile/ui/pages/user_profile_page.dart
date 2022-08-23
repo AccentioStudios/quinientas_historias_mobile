62469e2a363b7f0779b4deddd902280abc3c9e35
mcdiazgbravo <52355710+mcdiazgbravo@users.noreply.github.com>
2022-08-23 11:44:02 -0500

    feat(user_profile): modfiicacion user_profile_page

--- lib/features/profiles_module/user_profile/ui/pages/user_profile_page.dart
+++ lib/features/profiles_module/user_profile/ui/pages/user_profile_page.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quinientas_historias/core/ui/widgets/user_avatar.dart';
+import 'package:quinientas_historias/core/utils/constants.dart';
 import 'package:quinientas_historias/features/profiles_module/user_profile/ui/bloc/cubit/team_profile_cubit.dart';
+import 'package:quinientas_historias/features/profiles_module/user_profile/ui/widgets/profilepic-widget.dart';
 
+import '../../../../../core/data/entities/user_entity.dart';
 import '../../../../../core/ui/widgets/buttom_bar.dart';
 
 class UserProfilePage extends StatelessWidget {
-  const UserProfilePage({Key? key}) : super(key: key);
+  const UserProfilePage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +22,16 @@ class UserProfilePage extends StatelessWidget {
             onRefresh: () async {
               await Future.delayed(const Duration(seconds: 1));
             },
-            child: ListView(),
+            child: ListView(
+              children: <Widget>[
+                /*UserAvatar(
+                  user: user!,
+                ),*/
+                const SizedBox(
+                  width: Constants.space18,
+                ),
+              ],
+            ),
           ),
         );
       },
