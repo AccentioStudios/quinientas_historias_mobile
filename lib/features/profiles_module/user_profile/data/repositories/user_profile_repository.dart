4cfcab182c465331c23694131bf7f1ef5629ff85
Alvaro Gabriel Gomez <alvarogabrielgomez@gmail.com>
2022-08-23 15:09:18 -0300

    fix: fix endpoint of user profile

--- lib/features/profiles_module/user_profile/data/repositories/user_profile_repository.dart
+++ lib/features/profiles_module/user_profile/data/repositories/user_profile_repository.dart
@@ -3,8 +3,11 @@ import '../../../../../core/integrations/api_service/api_service.dart';
 
 class UserProfileRepository with ApiService {
   Stream<User> getUserProfile(int? userId) async* {
+    final endpoint =
+        userId != null ? '/v1/users/profile/$userId' : '/v1/users/profile';
+
     yield* appApi
-        .get('/v1/users/profile/$userId')
+        .get(endpoint)
         .handleJson(mapper: (json) => User.fromJson(json));
   }
 }
