b31176fd08fc66f3a4f57a50d2c42d1e1d70405a
mcdiazgbravo <52355710+mcdiazgbravo@users.noreply.github.com>
2022-08-25 17:52:40 -0500

    feat(user_profile): div icons, user profile header

--- lib/features/profiles_module/user_profile/ui/widgets/user_profile_header.dart
+++ lib/features/profiles_module/user_profile/ui/widgets/user_profile_header.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:quinientas_historias/core/data/entities/user_entity.dart';
+import 'package:quinientas_historias/core/ui/widgets/outlined_card.dart';
+import 'package:quinientas_historias/core/ui/widgets/padding_column.dart';
+import 'package:quinientas_historias/core/ui/widgets/percentage_progress_bar.dart';
 
 import '../../../../../core/ui/widgets/user_avatar.dart';
+import '../../../../../core/utils/colors.dart';
 import '../../../../../core/utils/constants.dart';
 
 class UserProfileHeader extends StatelessWidget {
@@ -35,32 +41,16 @@ class UserProfileHeader extends StatelessWidget {
             ],
           ),
           _UserProfile(
-              userNameProfileTextStyle: userNameProfileTextStyle, user: user)
+              userNameProfileTextStyle: userNameProfileTextStyle, user: user),
+          const Padding(
+            padding: EdgeInsets.only(bottom: Constants.space41),
+          ),
+          _UserDivision(
+            user: user,
+          ),
         ],
       ),
     );
-    /*return ListView(
-      children: <Widget>[
-        AppBar(
-          elevation: 0,
-          centerTitle: false,
-          toolbarHeight: 64,
-          actions: [
-            IconButton(
-              iconSize: 24,
-              onPressed: () {},
-              icon: const Icon(Icons.edit_outlined),
-            ),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text("Editar perfil"),
-            )
-          ],
-        ),
-        _UserProfile(
-            userNameProfileTextStyle: userNameProfileTextStyle, user: user)
-      ],
-    );*/
   }
 }
 
@@ -78,10 +68,24 @@ class _UserProfile extends StatelessWidget {
       children: <Widget>[
         Container(
           margin: const EdgeInsets.all(Constants.space18),
-          child: UserAvatar(
-            user: user!,
-            width: 140,
-            height: 140,
+          child: Stack(
+            alignment: AlignmentDirectional.bottomEnd,
+            children: <Widget>[
+              UserAvatar(
+                user: user!,
+                width: 140,
+                height: 140,
+              ),
+              const CircleAvatar(
+                backgroundColor: Color(0xff385775),
+                radius: 18.5,
+                child: Icon(
+                  Icons.camera_alt_outlined,
+                  size: 22.5,
+                  color: Colors.white,
+                ),
+              ),
+            ],
           ),
         ),
         Column(
@@ -93,5 +97,117 @@ class _UserProfile extends StatelessWidget {
         ),
       ],
     );
+  }
+}
+
+class _UserDivision extends StatelessWidget {
+  const _UserDivision({
+    Key? key,
+    this.user,
+  }) : super(key: key);
+
+  final User? user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          EdgeInsets.only(right: Constants.space16, left: Constants.space21),
+      child: OutlinedCard(
+        child: PaddingColumn(
+          padding: const EdgeInsets.all(Constants.space4),
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                const Text('Mi nivel: ',
+                    style:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+                SvgPicture.asset(
+                  getDivisionBadge(user?.division?.level),
+                  height: 30,
+                  width: 30,
+                ),
+                Text(getDivisionName(user?.division?.level)),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Flexible(
+                    child: PercentageProgressBar(
+                  backgroundColor: getDivisionColor(user!.division?.level),
+                  completedPercentage: 30,
+                )),
+                Padding(
+                  padding: const EdgeInsets.only(left: Constants.space8),
+                  child: const Text('30'),
+                )
+              ],
+            ),
+            /*PercentageProgressBar(
+              backgroundColor: getDivisionColor(user!.division?.level),
+              completedPercentage: 30,
+            ),*/
+          ],
+        ),
+      ),
+    );
+  }
+
+  String getDivisionBadge(int? level) {
+    if (level == 1) {
+      return 'assets/icons/division-1-badge.svg';
+    }
+    if (level == 2) {
+      return 'assets/icons/division-2-badge.svg';
+    }
+    if (level == 3) {
+      return 'assets/icons/division-3-badge.svg';
+    }
+    if (level == 4) {
+      return 'assets/icons/division-4-badge.svg';
+    }
+    if (level == 5) {
+      return 'assets/icons/division-5-badge.svg';
+    }
+    return '';
+  }
+
+  String getDivisionName(int? level) {
+    if (level == 1) {
+      return 'Iniciante';
+    }
+    if (level == 2) {
+      return 'Entusiasta';
+    }
+    if (level == 3) {
+      return 'Dedicado';
+    }
+    if (level == 4) {
+      return 'Lector Defnitivo';
+    }
+    if (level == 5) {
+      return 'Maestro';
+    }
+    return '';
+  }
+
+  Color getDivisionColor(int? level) {
+    if (level == 1) {
+      return division1Color;
+    }
+    if (level == 2) {
+      return division2Color;
+    }
+    if (level == 3) {
+      return division3Color;
+    }
+    if (level == 4) {
+      return division4Color;
+    }
+    if (level == 5) {
+      return division5Color;
+    }
+
+    return Colors.transparent;
   }
 }
